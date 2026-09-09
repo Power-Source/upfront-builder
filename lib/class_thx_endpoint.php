44d0c76db940a501c994059e02c64809db87dce9
DerN3rd <dern3rd@outlook.at>
2026-09-09 13:20:45 +0200

    Füge den Aufruf von upfront_boot_editor_trigger in die Methode start_editor ein, um den Editor-Modus zu initialisieren.

--- lib/class_thx_endpoint.php
+++ lib/class_thx_endpoint.php
@@ -43,6 +43,7 @@ abstract class Thx_VirtualSubpage extends Upfront_VirtualSubpage {
 
 	public function start_editor () {
 		upfront_exporter_clear_conversion_cache($this->get_slug());
+		echo upfront_boot_editor_trigger($this->get_editor_mode());
 		$show_exp = (int)get_user_option(Thx_Exposed::BOOTSTRAP_EXP_SLUG);
 		$active_theme = get_option('stylesheet');
 		echo '<script type="text/javascript">
